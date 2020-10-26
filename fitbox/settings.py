b8adc7257dd54d386b8deb79eedd20ced35a8510
ravellys <lucas.ravellys@ufpe.br>
2020-10-26 15:48:04 -0300

    add suprusr

--- fitbox/settings.py
+++ fitbox/settings.py
@@ -34,6 +34,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
@@ -139,7 +140,6 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=False)
 # STORAGE CONFIGURATION IN S3 AWS
 # ------------------------------------------------------------------------------
 if AWS_ACCESS_KEY_ID:
-    print(AWS_ACCESS_KEY_ID)
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
