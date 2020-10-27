1b044ab3017549feb30c8538a55ccd16d5340585
ravellys <lucas.ravellys@ufpe.br>
2020-10-27 18:30:00 -0300

    add indice de pacientes

--- fitbox/cadastro_paciente/urls.py
+++ fitbox/cadastro_paciente/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from fitbox.cadastro_paciente.views import cadastro_paciente, indice
+from fitbox.cadastro_paciente.views import cadastro_paciente
 
 app_name = 'cadastro'
 urlpatterns = [
